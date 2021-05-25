d63b05c422d05be05a47074e6f51e02dacb07e97
Laszlo Albert <albertlacka1@gmail.com>
2021-05-25 16:37:59 +0100

    Cleanup

--- details.js
+++ details.js
@@ -25,17 +25,17 @@ let teddyId = urlParams.get ('id'); // Optional - create a function that takes i
 const showTeddyData = (bears) => {
     for (let bear of bears) {
         if (bear._id == teddyId) {
-        document.querySelector('img').src = bear.imageUrl
-        document.getElementById('price').textContent = bear.price
-        document.getElementById('description').textContent = bear.description
-        const szinek = bear.colors
+            document.querySelector('img').src = bear.imageUrl
+            document.getElementById('price').textContent = bear.price
+            document.getElementById('description').textContent = bear.description
+
+            const szinek = bear.colors
+            
             szinek.forEach(color => {   
-        const selection = document.createElement('option')
-        selection.textContent = color;
-
-
-        const ddown = document.getElementById('dropdown')
-        ddown.appendChild(selection)
+                const selection = document.createElement('option')
+                selection.textContent = color;
+                const ddown = document.getElementById('dropdown')
+                ddown.appendChild(selection)
             }); 
             
             //Creating a link to the cart page
@@ -76,7 +76,6 @@ const showTeddyData = (bears) => {
 
             function addToBasket(){
                 
-
                 // get data from the API (the whole object that will be put into the basket)
                 let singleChosenProduct = bear;
 
@@ -105,18 +104,6 @@ const showTeddyData = (bears) => {
                 localStorage.setItem('totalPrice', JSON.stringify(totalPrice)); 
                 
             }
-            
-            
-            //function displayBasketContents(){
-
-
-                // If there is data to display
-            
-
-            // function removeFromBasket(){
-                //let idToRemove = list.findIndex
-            // }
-
         }      
     }
 }
